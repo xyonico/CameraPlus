1d110b5c8ba79cc370744a46257cddb6c79ffbdf
Rémi Lavergne <rremilavergne@gmail.com>
2018-05-22 19:48:37 +0200

    Add fixed camera option

--- CameraPlus/CameraPlusBehaviour.cs
+++ CameraPlus/CameraPlusBehaviour.cs
@@ -11,6 +11,10 @@ namespace CameraPlus
 		private float _fov;
 		private float _posSmooth;
 		private float _rotSmooth;
+
+        private bool _fixed;
+        private Vector3 _fixedPos;
+        private Quaternion _fixedRot;
 		
 		private void Awake()
 		{
@@ -55,16 +59,39 @@ namespace CameraPlus
 			_fov = Convert.ToSingle(Plugin.Ini.GetValue("fov", "", "90"));
 			_posSmooth = Convert.ToSingle(Plugin.Ini.GetValue("positionSmooth", "", "10"));
 			_rotSmooth = Convert.ToSingle(Plugin.Ini.GetValue("rotationSmooth", "", "5"));
+
+            _fixed = Convert.ToBoolean(Plugin.Ini.GetValue("fixed", "", "false"));
+            _fixedPos = new Vector3(
+                Convert.ToSingle(Plugin.Ini.GetValue("posx", "", "0")),
+                Convert.ToSingle(Plugin.Ini.GetValue("posy", "", "0")),
+                Convert.ToSingle(Plugin.Ini.GetValue("posz", "", "0"))
+                );
+
+            _fixedRot = new Quaternion(
+                Convert.ToSingle(Plugin.Ini.GetValue("rotx", "", "0")),
+                Convert.ToSingle(Plugin.Ini.GetValue("roty", "", "0")),
+                Convert.ToSingle(Plugin.Ini.GetValue("rotz", "", "0")),
+                Convert.ToSingle(Plugin.Ini.GetValue("rotw", "", "0"))
+                );
+
 			SetFOV();
 		}
 
-		private void LateUpdate()
-		{
-			var camera = MainCamera.transform;
-			
-			transform.position = Vector3.Lerp(transform.position, camera.position, _posSmooth * Time.deltaTime);
-			transform.rotation = Quaternion.Slerp(transform.rotation, camera.rotation, _rotSmooth * Time.deltaTime);
-		}
+        private void LateUpdate()
+        {
+            var camera = MainCamera.transform;
+
+            if (_fixed)
+            {
+                transform.position = _fixedPos;
+                transform.rotation = _fixedRot;
+                return;
+            }
+
+            transform.position = Vector3.Lerp(transform.position, camera.position, _posSmooth * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, camera.rotation, _rotSmooth * Time.deltaTime);
+
+        }
 
 		private void SetFOV()
 		{
